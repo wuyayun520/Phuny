7047ce39feff54eaf968c4db4cc689d01adc8fc6
Charlotte <18631656747@163.com>
2025-04-30 18:31:15 +0800

    Remove workflow files

--- ios/Runner/main.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import UIKit
-import FlieProvider
-
-UIApplicationMain(CommandLine.argc,
-                  CommandLine.unsafeArgv,
-                  NSStringFromClass(UtilizationKingfisherCompatible.self),
-                  NSStringFromClass(AppDelegate.self))
